b3acadd19aba48a37678a233d4509994f9c2c10c
Ivanmih99 <ivanmihajlovski123@gmail.com>
2022-12-09 17:46:57 +0100

    full working js

---
+++ Homework JS 2/app.js
@@ -0,0 +1,50 @@
+let year = prompt("What year were you born?");
+let result = (year - 4) % 12;
+
+if (result === 0) {
+  console.log("Rat");
+}
+
+if (result === 1) {
+  console.log("Ox");
+}
+
+if (result === 2) {
+  console.log("Tiger");
+}
+
+if (result === 3) {
+  console.log("Rabbit");
+}
+
+if (result === 4) {
+  console.log("Dragon");
+}
+
+if (result === 5) {
+  console.log("Snake");
+}
+
+if (result === 6) {
+  console.log("Horse");
+}
+
+if (result === 7) {
+  console.log("Goat");
+}
+
+if (result === 8) {
+  console.log("Monkey");
+}
+
+if (result === 9) {
+  console.log("Rooster");
+}
+
+if (result === 10) {
+  console.log("Dog");
+}
+
+if (result === 11) {
+  console.log("Pig");
+}
